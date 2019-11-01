8d3398eb7eb1f81a0890a2f8df59c880247d9c10
Trevor Davis <tzendou@gmail.com>
2019-11-01 15:11:21 -0600

    Initial commit

---
+++ src/main/java/com/trevor/chatroomjavaspringwebsocket/ChatroomJavaSpringWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.trevor.chatroomjavaspringwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomJavaSpringWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatroomJavaSpringWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trevor/chatroomjavaspringwebsocket/ChatroomJavaSpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevor.chatroomjavaspringwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomJavaSpringWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
