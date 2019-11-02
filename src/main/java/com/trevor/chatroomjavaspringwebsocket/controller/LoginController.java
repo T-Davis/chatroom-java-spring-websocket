b60b19766dbbeb25938f31afc299e12f0a3c3d7a
Trevor Davis <tzendou@gmail.com>
2019-11-02 17:06:46 -0600

    Refactor login controller

--- src/main/java/com/trevor/chatroomjavaspringwebsocket/controller/LoginController.java
+++ src/main/java/com/trevor/chatroomjavaspringwebsocket/controller/LoginController.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.net.UnknownHostException;
-
 @Controller
 public class LoginController {
     @RequestMapping({"/", "login"})
@@ -15,8 +12,4 @@ public class LoginController {
         return new ModelAndView("login");
     }
 
-    @RequestMapping("chat")
-    public ModelAndView chat(String username, HttpServletRequest request) throws UnknownHostException {
-        return new ModelAndView("chat");
-    }
 }
